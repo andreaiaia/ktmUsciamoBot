de21021a07e85c2f6fb5473fdfceb719d25fc542
andreaiaia <andreaiaia@hotmail.it>
2021-11-20 20:13:45 +0100

    experiments with simple commands

--- bot_main.py
+++ bot_main.py
@@ -1,20 +1,53 @@
-from telegram import Update
-from telegram.ext import Updater, CallbackContext, CommandHandler
+from telegram import Update, InlineQueryResultArticle, InputTextMessageContent
+from telegram.ext import Updater, CallbackContext, CommandHandler, MessageHandler, Filters, InlineQueryHandler
 import logging
 
 # Inserire il proprio token ottenuto dal BotFather
-TOKEN = "INSERISCI IL TOKEN"
-
-updater = Updater(TOKEN, use_context=True)
-dispatcher = updater.dispatcher
-
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                      level = logging.INFO)
+TOKEN = "INSERISCI QUI IL TOKEN"
 
 def start(update: Update, context: CallbackContext):
   context.bot.send_message(chat_id=update.effective_chat.id, text="Benvenuto in ktmUsciamoBot, io sono qui per aiutare voialtri ad organizzarvi per uscire. \n Usa il comando /help per vedere la lista di ciò che posso fare.")
 
-start_handler = CommandHandler('start', start)
-dispatcher.add_handler(start_handler)
+def echo(update: Update, context: CallbackContext):
+  context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
 
-updater.start_polling()
+def caps(update: Update, context: CallbackContext):
+  text_caps = ' '.join(context.args).upper()
+  context.bot.send_message(chat_id=update.effective_chat.id, text=text_caps)
+
+def inline_caps(update: Update, contect: CallbackContext):
+  query = update.inline_query.query
+  if not query:
+    return
+  results = []
+  results.append(
+    InlineQueryResultArticle(
+      id=query.upper(),
+      title='caps',
+      input_message_content=InputTextMessageContent(query.upper())
+    )
+  )
+  context.bot.answer_inline_query(update.inline_query.id, results)
+
+
+def main():
+  updater = Updater(TOKEN, use_context=True)
+  dispatcher = updater.dispatcher
+  logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                      level=logging.INFO)
+  
+  start_handler = CommandHandler('start', start)
+  dispatcher.add_handler(start_handler)
+
+  echo_handler = MessageHandler(Filters.text & (~Filters.command), echo)
+
+  caps_handler = CommandHandler('caps', caps)
+  dispatcher.add_handler(caps_handler)
+
+  inline_caps_handler = InlineQueryHandler(inline_caps)
+
+  updater.start_polling()
+  updater.idle()
+
+if __name__ == '__main__':
+  main()
