9f002ed442ef1f6d8579d8e0735439303eb8e16d
Andrea Bianchi <bianan96@gmail.com>
2021-11-25 21:11:24 +0100

    fixed indentation

--- bot_main.py
+++ bot_main.py
@@ -14,42 +14,42 @@ from src.eastereggs import dna, ping
 TOKEN = "YOUR TOKEN HERE"
 
 def main():
-  # Create the Updater and pass the Token
-  updater = Updater(TOKEN, use_context=True)
-  # Create a dispatcher variable for ease
-  dp = updater.dispatcher
-  # This prints error's log to help with debug
-  logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                      level=logging.INFO)
+    # Create the Updater and pass the Token
+    updater = Updater(TOKEN, use_context=True)
+    # Create a dispatcher variable for ease
+    dp = updater.dispatcher
+    # This prints error's log to help with debug
+    logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                        level=logging.INFO)
 
-  # Create the commands and add them to the dispatcher queue
-  # text commands (the ones with the /)
-  
-  # Basic commands - starting and using the bot
-  dp.add_handler(CommandHandler('start', simple_reply))
-  dp.add_handler(CommandHandler('help', simple_reply))
-  dp.add_handler(CommandHandler('wiki', simple_reply))
-  # New group member
-  add_group_handle = MessageHandler(Filters.status_update.new_chat_members, add_group)
-  dispatcher.add_handler(add_group_handle)
+    # Create the commands and add them to the dispatcher queue
+    # text commands (the ones with the /)
+    
+    # Basic commands - starting and using the bot
+    dp.add_handler(CommandHandler('start', simple_reply))
+    dp.add_handler(CommandHandler('help', simple_reply))
+    dp.add_handler(CommandHandler('wiki', simple_reply))
+    # New group member
+    add_group_handle = MessageHandler(Filters.status_update.new_chat_members, add_group)
+    dispatcher.add_handler(add_group_handle)
 
-  # Hangouts making
-  dp.add_handler(CommandHandler('usciamo', hangout))
-  dp.add_handler(CallbackQueryHandler(btn_join))
-  dp.add_handler(CommandHandler('join', join))
-  dp.add_handler(CommandHandler('abort', abort))
-  dp.add_handler(CommandHandler('quindi', summary))
+    # Hangouts making
+    dp.add_handler(CommandHandler('usciamo', hangout))
+    dp.add_handler(CallbackQueryHandler(btn_join))
+    dp.add_handler(CommandHandler('join', join))
+    dp.add_handler(CommandHandler('abort', abort))
+    dp.add_handler(CommandHandler('quindi', summary))
 
-  # Easter eggs
-  dp.add_handler(CommandHandler('dna', dna))
-  dp.add_handler(CommandHandler('ping', ping))
+    # Easter eggs
+    dp.add_handler(CommandHandler('dna', dna))
+    dp.add_handler(CommandHandler('ping', ping))
 
-  # Inline keyboard commands (the ones you can call with @)
-  
-  # Start the bot
-  updater.start_polling()
-  # Run the bot until user presses Crtl-C or SIGINT, SIGTERM or SIGABRT
-  updater.idle()
+    # Inline keyboard commands (the ones you can call with @)
+    
+    # Start the bot
+    updater.start_polling()
+    # Run the bot until user presses Crtl-C or SIGINT, SIGTERM or SIGABRT
+    updater.idle()
 
 if __name__ == '__main__':
-  main()
+    main()
--- src/eastereggs.py
+++ src/eastereggs.py
@@ -3,13 +3,13 @@ from telegram.ext import CallbackContext
 from src.helpers import (get, put)
 
 def dna(update: Update, context: CallbackContext) -> None:
-  key = str(update.message.from_user.id)
-  value = 0
-  if get(key, context):
-    value = int(get(key, context))
-  value += 1
-  put(key, value, context)
-  update.message.reply_text(f"Bidoni di Dna: {str(value)}.")
+    key = str(update.message.from_user.id)
+    value = 0
+    if get(key, context):
+        value = int(get(key, context))
+    value += 1
+    put(key, value, context)
+    update.message.reply_text(f"Bidoni di Dna: {str(value)}.")
 
 def ping(update: Update, _: CallbackContext) -> None:
-  update.message.reply_text("PONG.")
+    update.message.reply_text("PONG.")
